598d0dd8179574cdacb77431fd0d4797e48d2b2c
nghia1501 <nghianguyen15012004@gmail.com>
2026-05-11 12:39:07 +0700

    feat: Add ProfileCard and TeamProfiles components for team section
    - Implemented ProfileCard component to display individual team member details including name, role, location, and social links.
    - Created TeamProfiles component to render a list of team members using ProfileCard.
    - Added CSS styles for TeamProfiles and ProfileCard for improved layout and design.
    - Introduced TailwindButton component with animated gradient styling.
    - Created All_Products page skeleton.
    - Updated AppRouter to include new routes for All_Products and other components.

---
+++ src/pages/All_Products/All_Products.jsx
@@ -0,0 +1,14 @@
+// import { useTranslation } from 'react-i18next';
+
+function All_Products(){
+//   const { t } = useTranslation();
+
+    return(
+        <>
+            <h1>{t('product.allproducts')}</h1>
+        </>
+    )
+}
+
+export default All_Products;
+
